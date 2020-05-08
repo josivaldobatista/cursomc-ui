c4c2aaa7687c00231515cc49f7e71a92efdf2a82
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-05-08 18:26:36 -0300

    Tratando error 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -37,7 +37,13 @@ export class ProfilePage {
           this.cliente = response;
           this.getImageIfExist();
         },
-          error => { })
+          error => {
+            if(error.status == 403) {
+              this.navCtrl.setRoot('HomePage');
+            }
+          })
+    } else {
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
